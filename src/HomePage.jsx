cb0b82a618e6221a2592cdd255e71cb5deb64849
kingsley <kingsleykay26@gmail.com>
2026-02-26 18:45:10 +0100

    Base project setup

---
+++ src/HomePage.jsx
@@ -0,0 +1,17 @@
+export function HomePage(){
+  return (
+    <div>
+    <div>
+      <p>UKK</p>
+
+      <nav>
+        <li>Home</li>
+        <li>Work</li>
+        <li>Info</li>
+      </nav>
+
+      <p>[2026]</p>
+    </div>
+    </div>
+  )
+}
