4467d3b4cda8c3ea581584ba686752a47fe336ef
kingsley <kingsleykay26@gmail.com>
2026-03-06 17:38:38 +0100

    compiled entire section and added general style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import HomePage from "./HomePage";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import { ReactLenis } from "lenis/react";
+import Contact from "./sections/Contact";
+
 import "./reset.css";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <ReactLenis root />
+      <Hero />
+      <Projects />
+      <Contact />
     </>
   );
 }
