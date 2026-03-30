0919559a0fc418f2a60b9c351a10eaec418a7158
kingsley <kingsleykay26@gmail.com>
2026-03-30 02:59:24 +0100

    fix(project-card): replaced hardcoded fill on arrow svg with currentColor

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -54,13 +54,13 @@ export function ProjectCard({
               <div className="enter-svg-container">
                 <svg
                   viewBox="0 0 11 11"
-                  fill="none"
+                  fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   {" "}
                   <path
                     d="M4.80248 10.069L8.07867 7.24247H0V0H1.52629V5.92566H8.07867L4.80248 3.09912L5.88157 2.16813L11 6.58407L5.88157 11L4.80248 10.069Z"
-                    fill="black"
+                    fill="currentColor"
                   ></path>{" "}
                 </svg>
               </div>
