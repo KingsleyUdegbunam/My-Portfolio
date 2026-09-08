4446529acfd5dcdcf92b78322c01212094fe4225
kingsley <kingsleykay26@gmail.com>
2026-09-08 18:07:13 +0100

    chore: match remote foler name with local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Projects from "../components/web/sections/Projects";
 import Contact from "../components/web/sections/Contact";
 import Hero from "../components/web/sections/Hero";
+import Projects from "../components/web/sections/Projects";
 
 function App() {
   return (
