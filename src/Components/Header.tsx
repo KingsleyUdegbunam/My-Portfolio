9f47a19c1340fedc34a15cba739d457f62b65de1
kingsley <kingsleykay26@gmail.com>
2026-09-08 11:47:26 +0100

    refactor(header): update scroll handling with lenis and restructure elements

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,28 +1,24 @@
 "use client";
 
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useLenis } from "../provider/LenisContext";
 
 export default function Header() {
   const [navHidden, setNavHidden] = useState<boolean>(false);
-  const headerRef = useRef<HTMLElement | null>(null);
-
-  const verticalScrollOrigin = useRef<number | null>(null);
+  const lenis = useLenis();
 
   useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      verticalScrollOrigin.current = window.innerHeight * 1.1;
-
-      if (currentScrollY > verticalScrollOrigin.current) {
-        setNavHidden(true);
-      } else {
-        setNavHidden(false);
-      }
+    if (!lenis) return;
+    const handleScroll = ({ scroll }: { scroll: number }) => {
+      setNavHidden(scroll > window.innerHeight * 1.3);
     };
-    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    lenis.on("scroll", handleScroll);
+
+    return () => {
+      lenis.off("scroll", handleScroll);
+    };
+  }, [lenis]);
 
   const navArray = [
     { title: "HOME", link: "#" },
@@ -32,18 +28,30 @@ export default function Header() {
 
   return (
     <header
-      ref={headerRef}
-      className={`header bg-[linear-gradient(to_top,rgba(0,0,0,0),hsla(0,0%,0%,0.198))] backdrop-blur-[1px] flex justify-between py-[1.2rem] px-[0.7rem] md:px-4 fixed! w-full z-20! transition-transform duration-300 leading-none ${navHidden ? "-translate-y-25" : ""}`}
+      className={`fixed! text-white! top-0 z-30! w-full
+    flex justify-between
+    py-[1.2rem] px-[0.7rem] md:px-4
+    bg-[linear-gradient(to_top,rgba(0,0,0,0),hsla(0,0%,0%,0.198))]
+    backdrop-blur-[1px]
+    transition-transform duration-300 leading-none
+    ${navHidden ? "-translate-y-25" : ""}`}
     >
       <h1 className="font-koulen! text-[clamp(1rem,5vw,2.8rem)]">KAY</h1>
 
       <nav>
-        <ul className="mix-blend-difference flex flex-col gap-[0.4rem]">
+        <ul className="flex flex-col gap-4">
           {navArray.map((link) => (
-            <li className="hover:italic text-[12px] hover:list-[square] focus:italic focus:list-[square]">
-              <a key={link.title} href={link.link}>
+            <li
+              key={link.title}
+              className="text-[12px] tracking-tighter hover:list-[square] hover:italic"
+            >
+              <button
+                onClick={() => {
+                  lenis?.scrollTo(link.link);
+                }}
+              >
                 {link.title}
-              </a>
+              </button>
             </li>
           ))}
         </ul>
